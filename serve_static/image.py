76dbe41a1aee62e20f0fb3eb494213b1d2ccd1a0
lucemia <lucemia@gmail.com>
2014-12-30 20:39:34 +0800

    init project

---
+++ serve_static/image.py
@@ -0,0 +1,41 @@
+import cloudstorage as gcs
+from google.appengine.ext import blobstore
+from google.appengine.api import images
+from google.appengine.api import urlfetch
+import logging
+import hashlib
+
+def create_gs_file(bucket, filename, content):
+    with gcs.open("/%s/%s"%(bucket, filename), 'w') as f:
+        f.write(content)
+
+    return True
+
+
+def exist_gs_file(bucket, filename):
+    try:
+        gcs.stat("/%s/%s"%(bucket, filename))
+        return True
+    except gcs.NotFoundError:
+        return False
+
+def filehash(content):
+    m = hashlib.md5()
+    m.update(content)
+    return m.hexdigest()
+
+def cache_image(bucket, url, fp=None):
+    if not fp:
+        content = urlfetch.fetch(url, headers={'user-agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36'}).content
+    else:
+        content = fp.read()
+
+    filename = "img/" + filehash(content)
+    if exist_gs_file(bucket, filename):
+        blob_key = blobstore.create_gs_key("/gs/%s/%s" % (bucket, filename))
+    else:
+        create_gs_file(bucket, filename, content)
+        blob_key = blobstore.create_gs_key("/gs/%s/%s" % (bucket, filename))
+
+    return images.get_serving_url(blob_key)
+
---
+++ serve_static/test_image.py
@@ -0,0 +1,14 @@
+import os
+import unittest
+from google.appengine.ext import testbed
+
+class CacheImageTest(unittest.TestCase):
+    def setUp(self):
+        self.testbed = testbed.Testbed()
+        self.testbed.activate()
+        self.testbed.init_all_stubs()
+
+    def test_cache_image(self):
+        from image import cache_image
+        cache_image('test-bucket', "http://www.tagtoo.com.tw/static/website/img/member/Master.jpg")
+
