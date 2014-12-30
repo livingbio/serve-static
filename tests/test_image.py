32467def9b761889cf15ff3db998511863b09ac6
lucemia <lucemia@gmail.com>
2014-12-30 22:40:29 +0800

    change import path

--- tests/test_image.py
+++ tests/test_image.py
@@ -9,9 +9,9 @@ class CacheImageTest(unittest.TestCase):
         self.testbed.init_all_stubs()
 
     def test_cache_image(self):
-        from image import cache_image
+        from serve_static.image import cache_image
         import urllib
         url1 = cache_image('test-bucket', "http://www.tagtoo.com.tw/static/website/img/member/Master.jpg")
         url2 = cache_image('test-bucket', 'http://www.tagtoo.com.tw/static/website/img/member/Master.jpg', urllib.urlopen('http://www.tagtoo.com.tw/static/website/img/member/Master.jpg'))
         self.assertEqual(url1, url2)
-        
+
